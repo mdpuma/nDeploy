f1c678f08fc5be1f55c3410904cd63028ee8c4f8
pumamd <pumamd@yandex.ru>
2016-01-15 20:12:45 +0200

    * add reload for nginx restricted at once in 30 seconds (fix for too fast reloading, requested by cpanel update or something else)

--- scripts/reload_nginx.sh
+++ scripts/reload_nginx.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-ulimit -n 10000
+ulimit -n 50000
+LAST_RELOAD=$(cat /opt/nDeploy/lock/nginx.lastreload 2>/dev/null)
+
+# $LAST_RELOAD + 30 > CURRENT_TIME
+if [ -n "$LAST_RELOAD" ] && [ $((LAST_RELOAD + 30)) -gt "`date +%s`" ]; then
+	echo "[`date`] Last reload is at `date --date="@$LAST_RELOAD"`, skipping reload"
+	exit 0
+fi
+
+
 if [ -d /etc/systemd ]; then
 	/usr/sbin/nginx -t && systemctl reload nginx
 else
 	/usr/sbin/nginx -t && /etc/init.d/nginx reload
 fi
+echo $(date +%s) > /opt/nDeploy/lock/nginx.lastreload
