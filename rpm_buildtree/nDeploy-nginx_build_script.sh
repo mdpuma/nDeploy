3cba549182d001495e3b81b08367646d56eb741e
pumamd <pumamd@yandex.ru>
2016-01-15 11:59:53 +0200

    * updates & fixes

--- rpm_buildtree/nDeploy-nginx_build_script.sh
+++ rpm_buildtree/nDeploy-nginx_build_script.sh
@@ -2,7 +2,7 @@
 #Author: Anoop P Alias
 
 ##Vars
-NGINX_VERSION="1.9.5"
+NGINX_VERSION="1.9.9"
 NGINX_RPM_ITER="7.el6"
 #NPS_VERSION="1.9.32.6"
 #MY_RUBY_VERSION="2.2.3"
@@ -10,7 +10,9 @@ NGINX_RPM_ITER="7.el6"
 #CACHE_PURGE_VERSION="2.3"
 #NAXSI_VERSION="0.54"
 
-rm -f nginx-pkg-64/nginx-nDeploy*
+CPU_COUNT=$(ls /sys/devices/system/cpu/cpu[0-9]* -d | wc -l)
+
+rm -f nginx-pkg-64/nginx-nDeploy*rpm
 rm -rf nginx-${NGINX_VERSION}*
 
 rsync -av --exclude 'usr/lib' nginx-pkg-64-common/ nginx-pkg-64/
@@ -45,9 +47,8 @@ cd nginx-${NGINX_VERSION}/
 
 git clone https://github.com/kyprizel/testcookie-nginx-module.git
 
-
-./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --with-file-aio --with-ipv6 --with-http_v2_module --with-http_geoip_module --add-module=testcookie-nginx-module --with-cc-opt='-O2 -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic'
-make DESTDIR=./tempo install
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nobody --group=nobody --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --with-file-aio --with-ipv6 --with-http_v2_module --with-http_geoip_module --with-threads --add-module=testcookie-nginx-module --with-cc-opt='-O2 -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic'
+make DESTDIR=./tempo -j$CPU_COUNT install
 
 #git clone https://github.com/nbs-system/naxsi-rules.git
 #rsync -av naxsi-rules/*.rules ../nginx-pkg-64/etc/nginx/conf.d/
@@ -70,4 +71,4 @@ mkdir -p var/run
 chmod +x etc/rc.d/init.d/nginx
 chmod 644 etc/nginx/testcookie/testcookie_html/*
 
-fpm -s dir -t rpm -C ../nginx-pkg-64 --vendor "AMPLICA" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m admin@amplica.md -e --description "nDeploy custom nginx package" --url http://amplica.md --conflicts nginx -d zlib -d openssl -d pcre -d libcurl --after-install ../after_nginx_install --before-remove ../after_nginx_uninstall --name nginx-nDeploy .
+fpm -s dir -t rpm -C ../nginx-pkg-64 --vendor "AMPLICA" --version ${NGINX_VERSION} --iteration ${NGINX_RPM_ITER} -a $(arch) -m admin@amplica.md -e --description "nDeploy custom nginx package" --url http://amplica.md --conflicts nginx -d zlib -d openssl -d pcre -d libcurl --config-files /etc/nginx --after-install ../after_nginx_install --before-remove ../after_nginx_uninstall --name nginx-nDeploy .
