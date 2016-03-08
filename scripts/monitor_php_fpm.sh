a0f14d28ad067a3baa7e49f88b45a521ef2b7bd3
pumamd <pumamd@yandex.ru>
2016-03-08 17:41:40 +0200

    * added checking of nginx process and ndeploy_watcher

--- scripts/monitor_php_fpm.sh
+++ scripts/monitor_php_fpm.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-check() {
-	VER=$1
-	URL=http://localhost:808/ping$1
-	RESPONSE=`curl $URL 2>/dev/null`
+PHPFPM_BACKENDS="5.4 5.5 5.6 7.0"
+
+check_nginx() {
+	URL=http://localhost:808/nginx-status
+	RESPONSE=`curl --max-time 15 $URL 2>/dev/null | xargs`
 	echo "[`date`] URL=$URL Response=$RESPONSE"
-	if [ "$RESPONSE" != 'pong' ]; then
-		/opt/nDeploy/scripts/init_backends.pl --action=restart --forced --php=$VER
+	echo $RESPONSE | grep -E "^Active connections:" >/dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		[[ -d /etc/systemd ]] && systemctl restart nginx || /etc/init.d/nginx restart
+		echo 'Restarted nginx' 1>&2
 	fi
 }
 
-#check 5.4
-#check 5.5
-#check 5.6
-#check 7.0
+check_phpfpm() {
+	for VER in $PHPFPM_BACKENDS; do
+		URL=http://localhost:808/ping$VER
+		RESPONSE=`curl --max-time 15 $URL 2>/dev/null`
+		echo "[`date`] URL=$URL Response=$RESPONSE"
+		if [ "$RESPONSE" != 'pong' ]; then
+			/opt/nDeploy/scripts/init_backends.pl --action=restart --forced --php=$VER
+			echo "Restarted php-$VER" 1>&2
+		fi
+	done
+}
+
+check_watcher() {
+	pgrep -F /opt/nDeploy/watcher.pid >/dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		[[ -d /etc/systemd ]] && systemctl restart ndeploy_watcher || /etc/init.d/ndeploy_watcher restart
+		echo 'Restarted ndeploy_watcher' 1>&2
+	fi
+}
+
+check_nginx
+check_phpfpm
+check_watcher
