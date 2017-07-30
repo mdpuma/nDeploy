14f9f0416ec00d8d3f159186f3373c898d737525
pumamd <pumamd@yandex.ru>
2017-07-30 22:11:25 +0300

    * updated version

--- rpm_buildtree/nDeploy_build_script.sh
+++ rpm_buildtree/nDeploy_build_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Author: Anoop P Alias
 
-RPM_ITERATION="46"
+RPM_ITERATION="47"
 
 rm -f nDeploy-pkg/nDeploy-*rpm nDeploy-pkg-centos7/nDeploy-*rpm
 for version in nDeploy-pkg nDeploy-pkg-centos7; do
@@ -16,9 +16,9 @@ done
 
 cd nDeploy-pkg
 chmod 755 opt/nDeploy/scripts/* opt/nDeploy/apache_fpm_cp/* opt/nDeploy/nDeploy_cp/* -v
-fpm -s dir -t rpm -C ../nDeploy-pkg --vendor "AMPLICA" --iteration ${RPM_ITERATION}.el6 -d python-inotify -d python-jinja2 -d python-simplejson -d nginx-nDeploy -d python-argparse -d PyYAML -d perl-YAML-Tiny.noarch -d python-lxml -a noarch -m admin@amplica.md -e --description "nDeploy cPanel plugin" --url http://amplica.md --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-pkg --vendor "AMPLICA" --iteration ${RPM_ITERATION}.el6 -d python-inotify -d python-jinja2 -d python-simplejson -d nginx-nDeploy -d python-argparse -d PyYAML -d perl-YAML-Tiny -d python-lxml -a noarch -m admin@amplica.md -e --description "nDeploy cPanel plugin" --url http://amplica.md --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
 
 cd ../nDeploy-pkg-centos7
 chmod 755 opt/nDeploy/scripts/* opt/nDeploy/apache_fpm_cp/* opt/nDeploy/nDeploy_cp/* -v
-fpm -s dir -t rpm -C ../nDeploy-pkg-centos7 --vendor "AMPLICA" --iteration ${RPM_ITERATION}.el7 -d python-inotify -d python-jinja2 -d python-simplejson -d nginx-nDeploy -d python-argparse -d PyYAML -d perl-YAML-Tiny.noarch -d python-lxml -a noarch -m admin@amplica.md -e --description "nDeploy cPanel plugin" --url http://amplica.md --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
+fpm -s dir -t rpm -C ../nDeploy-pkg-centos7 --vendor "AMPLICA" --iteration ${RPM_ITERATION}.el7 -d python-inotify -d python-jinja2 -d python-simplejson -d nginx-nDeploy -d python-argparse -d PyYAML -d perl-YAML-Tiny -d python-lxml -a noarch -m admin@amplica.md -e --description "nDeploy cPanel plugin" --url http://amplica.md --after-install ../after_ndeploy_install --before-remove ../after_ndeploy_uninstall --name nDeploy .
 cd ..
