4f8c5a80fefe73b318723a490597e4b5686820de
pumamd <pumamd@yandex.ru>
2017-10-08 11:17:14 +0300

    * updates

--- scripts/monitor_apache.php
+++ scripts/monitor_apache.php
@@ -1,38 +1,73 @@
 #!/usr/bin/env php
 <?php
 
-$apache_status_url = 'http://127.0.0.1:8000/whm-server-status';
-$max_processes = 150;
-
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-// get status
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $apache_status_url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-$output = curl_exec($ch);
-curl_close($ch);
+check_apache('http://127.0.0.1:8000/whm-server-status', 150);
 
-// parse status
-$lines = explode("\n", $output);
-foreach($lines as $l) {
-	if(preg_match("/([0-9]+) requests currently being processed,/", $l, $matches)) {
-		$processes = $matches[1];
-		break;
-	}
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function check_apache($url, $max_processes) {
+    $status = curl_simple($url);
+    $pidfile = '/var/run/apache2/httpd.pid';
+//     print_stdout("apache_status is '$status'");
+    
+    // parse status
+    $found=0;
+    $lines = explode("\n", $status);
+    foreach($lines as $l) {
+        if(preg_match("/([0-9]+) requests currently being processed,/", $l, $matches)) {
+            $processes = $matches[1];
+            $found=1;
+            break;
+        }
+    }
+    if($found!=1) {
+        print_stderr("cant find processes count, force restart apache");
+        restart_apache();
+    } else {
+        // do action
+        if($processes >= $max_processes) {
+            print_stderr("httpd is running within limit of processes (".$processes."/".$max_processes.")");
+            print_stderr("restarting apache");
+            restart_apache();
+        } else {
+            print_stdout("httpd is running within limit of processes (".$processes."/".$max_processes.")");
+        }
+    }
+    $pid = trim(file_get_contents($pidfile));
+    system("ps -p $pid >/dev/null 2>&1", $ret);
+    if($ret==0) {
+        print_stdout("apache is running, pid=$pid");
+    }
+}
+function restart_apache() {
+    system("killall httpd -9 -v");
+    if(is_dir('/etc/systemd')) {
+        system('systemctl restart httpd');
+    } else {
+        system('/etc/init.d/httpd restart');
+    }
+}
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function curl_simple($url, $timeout=10, $connect_timeout=10) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $connect_timeout);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+function print_stderr($msg) {
+    $date = date("d-m-Y h:i");
+    file_put_contents('php://stderr', '['.$date.'] '.$msg."\n");
+}
+function print_stdout($msg) {
+    $date = date("d-m-Y h:i");
+    echo('['.$date.'] '.$msg."\n");
 }
 
-// do action
-if($processes >= $max_processes) {
-	system("killall httpd -9 -v");
-	system("systemctl restart httpd");
-	file_put_contents('php://stderr', "httpd is running within limit of processes (".$processes."/".$max_processes.")\n");
-
-} else {
-	echo("httpd is running within limit of processes (".$processes."/".$max_processes.")\n");
-}
 ?>
---
+++ scripts/monitor_php_fpm.php
@@ -0,0 +1,142 @@
+#!/usr/bin/env php
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+check_watcher();
+$ret = check_nginx('http://127.0.0.1:808/nginx-status');
+if($ret == 0) check_phpfpm(['5.4', '5.6', '7.0']);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function check_watcher() {
+    $pidfile = '/opt/nDeploy/watcher.pid';
+    if(is_file($pidfile)) {
+        $pid = trim(file_get_contents($pidfile));
+        system("ps -p $pid >/dev/null 2>&1", $ret);
+        if($ret != 0) {
+            restart_watcher();
+        }
+    } else {
+        restart_watcher();
+    }
+}
+function restart_watcher() {
+    print_stderr("Try to restart ndeploy_watcher");
+    if(is_dir("/etc/systemd")) {
+        system("systemctl restart ndeploy_watcher");
+    } else {
+        system("/etc/init.d/ndeploy_watcher restart");
+    }
+}
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function check_nginx($status_url) {
+    $status = curl_simple($status_url);
+    $status = str_replace("\n", '', $status);
+    print_stdout("nginx_status=$status");
+    if(!preg_match("/Active connections:/", $status)) {
+        return restart_nginx();
+    }
+    return 0;
+}
+function restart_nginx() {
+    print_stderr("Try to restart nginx");
+    $tries=4;
+    while($tries>0) {
+        if(is_dir("/etc/systemd")) {
+            system("systemctl restart nginx", $ret);
+        } else {
+            system("/etc/init.d/nginx restart", $ret);
+        }
+        print_stderr("return code is $ret");
+        if($ret==0) {
+            print_stdout("successfully restarted nginx");
+            return $ret;
+            break;
+        }
+        $tries--;
+        sleep(1);
+    }
+    return $ret;
+}
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function check_phpfpm($php_versions=array()) {
+    foreach($php_versions as $version) {
+        $status = curl_simple('http://127.0.0.1:808/ping'.$version);
+        print_stdout("phpfpm_$version=$status");
+        if($status != 'pong') {
+            restart_phpfpm($version);
+        }
+    }
+    
+    sleep(1);
+    $yaml_array = read_yaml('/opt/nDeploy/conf/backends.yaml');
+    foreach($yaml_array as $version => $path) {
+        $pidfile = $path.'/var/run/php-fpm.pid';
+        if(is_file($pidfile)) {
+            $pid = trim(file_get_contents($pidfile));
+            system("ps -p $pid >/dev/null 2>&1", $ret);
+            if($ret != 0) {
+                print_stderr("no existent process by pidfile $pidfile");
+                restart_phpfpm($version);
+            } else {
+                print_stdout("phpfpm_$version process exists");
+            }
+        } else {
+            print_stderr("no pidfile $pidfile");
+            restart_phpfpm($version);
+        }
+    }
+}
+function restart_phpfpm($version) {
+    print_stderr("Try to restart phpfpm_$version");
+    system("/opt/nDeploy/scripts/init_backends.pl --action=restart --forced --php=$version >/dev/null");
+}
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function curl_simple($url, $timeout=10, $connect_timeout=10) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $connect_timeout);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+function print_stderr($msg) {
+    $date = date("d-m-Y h:i");
+    file_put_contents('php://stderr', '['.$date.'] '.$msg."\n");
+}
+function print_stdout($msg) {
+    $date = date("d-m-Y h:i");
+    echo('['.$date.'] '.$msg."\n");
+}
+
+// PHP:
+//   php-5.3.29: /usr/local/phpbrew/php/php-5.3.29
+//   php-5.4.45: /usr/local/phpbrew/php/php-5.4.45
+//   php-5.6.30: /usr/local/phpbrew/php/php-5.6.30
+//   php-7.0.12: /usr/local/phpbrew/php/php-7.0.12
+// PROXY:
+//   apache: '8000'
+
+function read_yaml($config) {
+    if(!is_file($config)) {
+        print_stderr("cant open file $config");
+    }
+    $content = file_get_contents($config);
+    $content = explode("\n", $content);
+    $result='';
+    foreach($content as $i => $line) {
+        if(empty($line)) continue;
+        list($var1, $var2) = @explode(':', $line);
+        $var1=trim($var1);
+        $var2=trim($var2);
+        if(substr($var1,0,3) !== 'php') continue;
+        $var1=substr($var1,4);
+        $result[$var1]=$var2;
+    }
+    return $result;
+}
+?>
