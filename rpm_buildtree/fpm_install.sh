25e2b2e3b2b1ed2e3e748b2a455161be26f64d08
Dan Bilici <admin@iphost.md>
2021-04-14 11:45:55 +0300

    * updated fpm installer

--- rpm_buildtree/fpm_install.sh
+++ rpm_buildtree/fpm_install.sh
@@ -3,4 +3,5 @@
 yum install ruby ruby-devel rubygems rpm-build make gcc -y
 
 gem install json -v 1.8.6 -V
-gem install fpm -v 1.4.0 -V
+gem install childprocess -v 1.0.1 -V
+gem install fpm -v 1.10.2 -V
